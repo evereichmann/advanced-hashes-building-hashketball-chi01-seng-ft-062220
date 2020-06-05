ab1ff0334144fc370e3a00915bb4af879bd2607a
Eve Reichmann <ereichmann4@aol.com>
2020-06-05 20:27:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,8 +1,8 @@
 # Write your code here!
 def game_hash
- game_hash = 
- { home: {
-    :team_name => {} 
+ game_hash = { 
+   home: {
+    team_name: " " 
     #key points to a string of the team name
     :colors => {}
    # key points to an array of strings that are that team's colors
