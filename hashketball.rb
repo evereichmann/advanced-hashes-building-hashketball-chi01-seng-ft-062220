bda2d003beb1c6ca200b83cf7ab9509180ca108c
Eve Reichmann <ereichmann4@aol.com>
2020-06-05 20:45:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -51,7 +51,7 @@ def game_hash
       :assists => 2,
       :steals => 4,
       :blocks => 11,
-      :slam_dunks => 1},
+      :slam_dunks => 1}
       },
      #key points to an Array of Hashes. Each Hash in the Array should contain the players' stats. Each stat should be a key that points to a value for that stat. The keys should be
  }
