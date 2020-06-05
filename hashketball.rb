d65ea8fe81d71e8e1258244199c01287790462a7
Eve Reichmann <ereichmann4@aol.com>
2020-06-05 20:39:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -56,7 +56,7 @@ def game_hash
      #key points to an Array of Hashes. Each Hash in the Array should contain the players' stats. Each stat should be a key that points to a value for that stat. The keys should be
  }
   :away => {
-    :team_name => " ", 
+    :team_name => "Charlotte Hornets", 
     :colors => [],
     :players => {
       {:player_name => ,
