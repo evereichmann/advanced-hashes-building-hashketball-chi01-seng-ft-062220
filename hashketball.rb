75d373defd30fa993d820fb58fbd088c9c60080d
Eve Reichmann <ereichmann4@aol.com>
2020-06-05 21:00:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -105,9 +105,10 @@ def game_hash
       :assists => 12,
       :steals => 7,
       :blocks => 5,
-      :slam_dunks => 12}
-      },
- },
+      :slam_dunks => 12
+      }
+    }
+  }
  }
  end
  game_hash
