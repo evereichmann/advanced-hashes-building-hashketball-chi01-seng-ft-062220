bb0eecedf85b52445bf5d5e30a0430d2044f6d09
Eve Reichmann <ereichmann4@aol.com>
2020-06-05 20:50:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -54,7 +54,7 @@ def game_hash
       :slam_dunks => 1}
       },
      #key points to an Array of Hashes. Each Hash in the Array should contain the players' stats. Each stat should be a key that points to a value for that stat. The keys should be
- 
+    }
   :away => {
     :team_name => "Charlotte Hornets", 
     :colors => ["Turquoise", "Purple"],
@@ -106,7 +106,6 @@ def game_hash
       :slam_dunks => 12}
       },
  }
-   }
  end
  game_hash
   
