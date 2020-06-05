5a7f691c7a64cd82b91321d88ac8bb20e3d2711a
Eve Reichmann <ereichmann4@aol.com>
2020-06-05 20:31:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,22 +7,31 @@ def game_hash
     :colors => [],
    # key points to an array of strings that are that team's colors
     :players => {
-      :player_name
-      :number
-      :shoe
-      :points
-      :rebounds
-      :assists
-      :steals
-      :blocks
-      :slam_dunks
+      :player_name => ,
+      :number => ,
+      :shoe => ,
+      :points => ,
+      :rebounds => ,
+      :assists => ,
+      :steals => ,
+      :blocks => ,
+      :slam_dunks => ,
       },
      #key points to an Array of Hashes. Each Hash in the Array should contain the players' stats. Each stat should be a key that points to a value for that stat. The keys should be
  }
   :away => {
-    :team_name => {}
-    :colors => {}
-    :players => {}
-  }
+    :team_name => " ", 
+    :colors => [],
+    :players => {
+      :player_name => ,
+      :number => ,
+      :shoe => ,
+      :points => ,
+      :rebounds => ,
+      :assists => ,
+      :steals => ,
+      :blocks => ,
+      :slam_dunks => ,
+      },
  }
   
